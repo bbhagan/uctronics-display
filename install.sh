f5b91d5b3a128546813d401c7e28f7519b7e66a3
Brian Hagan <bbhagan@gmail.com>
2023-10-23 14:25:27 -0700

    Fixed /etc issue

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 echo "Cloning Uctronics & Install"
 git clone https://github.com/bbhagan/uctronics-display.git
 git clone https://github.com/UCTRONICS/U6143_ssd1306.git
-cd ./U6143_ssd1306/C
 
 echo "Compile code"
+cd ./U6143_ssd1306/C
 sudo make clean && sudo make 
 cd ../..
 
@@ -14,7 +14,7 @@ sudo cp -R ./U6143_ssd1306 /usr/local/bin/
 
 echo "Copy service to /etc/"
 cd ./uctronics-display
-sudo cp ./uctronics-display.service /systemd/system/uctronics-display.service
+sudo cp ./uctronics-display.service /etc/systemd/system/uctronics-display.service
 
 echo "Making a service and starting"
 sudo systemctl enable uctronics-display
